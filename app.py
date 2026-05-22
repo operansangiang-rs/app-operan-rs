ee579555f09a94703c81f31e9a476c036e00fc3d
operansangiang-rs <operan.sangiang@gmail.com>
2026-05-22 10:06:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -199,8 +199,8 @@ df = pd.read_sql_query("""
 SELECT *
 FROM operan
 WHERE unit = ?
+AND julianday('now') - julianday(tanggal) <= 7
 ORDER BY id DESC
-LIMIT 100
 """, conn, params=(selected_unit,))
 
 for _, r in df.iterrows():
